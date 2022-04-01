4c8d7a935fec269e208c4e1670987bc8427eb775
gusdn0108 <gusdn6671@naver.com>
2022-04-01 11:04:56 +0900

    댓글

--- back/db.js
+++ back/db.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 const mysql = require('mysql2')
 
 const host = process.env.DB_HOST || 'localhost'
-const user = process.env.DB_USER || 'ingoo'
-const password = process.env.DB_PASSWORD || 'ingoo'
+const user = process.env.DB_USER || 'gusdn6671'
+const password = process.env.DB_PASSWORD || 'Gusdn6671****'
 const database = process.env.DB_DATABASE || 'jenny'
 
 const config = {host,user,password,database}
