4e4ed4690d7693a6e2c008b2c5b408e11bb985eb
easy-young <leejy230698@naver.com>
2022-04-04 18:26:03 +0900

    front3 ADD

---
+++ front2/public/function/user.js
@@ -0,0 +1,13 @@
+function logout() {
+    const logout = document.querySelector('#logout');
+    logout.addEventListener('click', async (e) => {
+        e.preventDefault();
+        const option = {
+            'Content-type':'application/json',
+            withCredentials:true,
+        };
+        const response = await axios.post('http://localhost:3000/user/logout', null, option);
+        alert(`로그아웃 되었습니다.`);
+        location.href = 'http://localhost:3001';
+    });
+};
