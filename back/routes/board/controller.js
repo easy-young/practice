4a1048a692a1e25472a9aa08d9ac1687247685bf
easy-young <leejy230698@naver.com>
2022-04-10 15:29:45 +0900

    좋아요, 스크랩 완료

--- back/routes/board/controller.js
+++ back/routes/board/controller.js
@@ -9,10 +9,12 @@ exports.list = async (req, res) => {
             const posts = [];
             for (let i = 0; i < array.length; i++) {
                 const post = array[i];
+                const [[userdata]] = await pool.query(`SELECT * FROM user where userid=?`, [post.userid]);
                 const [tag] = await pool.query(`SELECT * FROM tag where idx=?`, [post.idx,]);
                 await posts.push({
                     ...post,
                     tag: tag,
+                    userimage: userdata.userimage,
                     date: await setDateChanger(post.date),
                 });
             }
@@ -199,27 +201,82 @@ exports.commentDelete = async (req, res) => {
 
 exports.good = async (req, res) => {
     try {
-        const data = await pool.query("SELECT * FROM board WHERE idx=?", req.params.idx);
+        const {idx} = req.body;
+        const data = await pool.query("SELECT * FROM board WHERE idx=?", idx);
         const responseData = data[0][0];
-        const goodUsers = data[0][0].goodUsers.split(",");
-        const isGood = goodUsers.findIndex((f) => f === req.user.userid) === -1 ? false : true;
-        if (isGood) {res.status(200).json({
-                                            reqName: "board_view",
-                                            status: false,
-                                            message: "이미 좋아요를 클릭하셨어요!",
-                                        });
+        if (responseData.goodUsers !== null) {
+            const goodUsers = responseData.goodUsers.split(",");
+            const isGood = goodUsers.findIndex((f) => f === req.user.userid) === -1 ? false : true;
+            if (isGood) {res.status(200).json({
+                                                reqName: "board_view",
+                                                status: false,
+                                                message: "이미 좋아요를 클릭하셨어요!",
+                                            });
+            } else {
+                const goodUsersString = responseData.goodUsers + ',' + req.user.userid;
+                responseData.good = responseData.good + 1;
+                const getData = await pool.query(`UPDATE board SET goodUsers=?, good =? WHERE idx =?`, [goodUsersString, responseData.good, idx]);
+                res.status(200).json({
+                    reqName: "board_view",
+                    status: true,
+                    data: responseData,
+                    goodUsers: responseData.goodUsers.split(","),
+                });
+            }
         } else {
-            const goodUSersString = data[0][0].goodUsers + req.user.userid;
+            const goodUsersString = req.user.userid + ',';
             responseData.good = responseData.good + 1;
-            await pool.query(`UPDATE board SET goodUsers='${goodUSersString}' good = '${responseData.good}' WHERE idx = '${req.params.idx}'`);
+            const getData = await pool.query(`UPDATE board SET goodUsers=?, good=? WHERE idx=?`, [goodUsersString, responseData.good, idx]);
             res.status(200).json({
                 reqName: "board_view",
                 status: true,
                 data: responseData,
-                goodUsers: data[0][0].goodUsers.split(","), //array
+                goodUsers: goodUsersString.split(',')
             });
         }
     } catch (error) {
+        console.log(error.message);
+        res.status(200).json({ reqName: "board_view", status: false });
+    }
+};
+
+exports.scrap = async (req, res) => {
+    try {
+        const {idx} = req.body;
+        const data = await pool.query("SELECT * FROM board WHERE idx=?", idx);
+        const responseData = data[0][0];
+        if (responseData.scrapUsers !== null) {
+            const scrapUsers = responseData.scrapUsers.split(",");
+            const isScrap = scrapUsers.findIndex((f) => f === req.user.userid) === -1 ? false : true;
+            if (isScrap) {res.status(200).json({
+                                                reqName: "board_view",
+                                                status: false,
+                                                message: "이미 스크랩을 클릭하셨어요!",
+                                            });
+            } else {
+                const scrapUsersString = responseData.scrapUsers + ',' + req.user.userid;
+                responseData.scrap = responseData.scrap + 1;
+                const getData = await pool.query(`UPDATE board SET scrapUsers=?, scrap=? WHERE idx =?`, [scrapUsersString, responseData.scrap, idx]);
+                res.status(200).json({
+                    reqName: "board_view",
+                    status: true,
+                    data: responseData,
+                    scrapUsers: responseData.scrapUsers.split(","),
+                });
+            }
+        } else {
+            const scrapUsersString = req.user.userid + ',';
+            responseData.scrap = responseData.scrap + 1;
+            const getData = await pool.query(`UPDATE board SET scrapUsers=?, scrap=? WHERE idx=?`, [scrapUsersString, responseData.scrap, idx]);
+            res.status(200).json({
+                reqName: "board_view",
+                status: true,
+                data: responseData,
+                scrapUsers: scrapUsersString.split(',')
+            });
+        }
+    } catch (error) {
+        console.log(error.message);
         res.status(200).json({ reqName: "board_view", status: false });
     }
 };
